e65a0cc306d6d7ba4ce0ae316b41c00980985824
Kevin Wang <kevwang@f6r9s16.clusters.42paris.fr>
2025-11-05 18:04:02 +0100

    oui

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -10,23 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+// #ifndef GET_NEXT_LINE_H
+// # define GET_NEXT_LINE_H
 
-# include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
+// # include <stdio.h>
+// # include <unistd.h>
+// # include <fcntl.h>
+// # include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
-# endif
+// # ifndef BUFFER_SIZE
+// #  define BUFFER_SIZE 1
+// # endif
 
-char	*get_next_line(int fd);
-int		ft_strlen(char *str);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_calloc(int size);
-int		ft_check(char *str, char c);
-char	*ft_erreur(int readfd, char *str);
+// char	*get_next_line(int fd);
+// int		ft_strlen(char *str);
+// char	*ft_strjoin(char *s1, char *s2);
+// char	*ft_calloc(int size);
+// int		ft_check(char *str, char c);
+// char	*ft_erreur(int readfd, char *str);
 
-#endif
+// #endif
